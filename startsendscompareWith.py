71cc557f5767601a1b351a00b8d9c93e8b5d9a23
sandhya <145090559+sandhyaakkamgari@users.noreply.github.com>
2024-08-04 11:53:52 +0530

    Add files via upload

---
+++ startsendscompareWith.py
@@ -0,0 +1,12 @@
+text = "Hello, World!"
+print(text.startswith("Hello")) 
+print(text.startswith("Goodbye")) 
+
+text = "Hello, World!"
+print(text.endswith("World!")) 
+print(text.endswith("Python")) 
+
+str1 = "apple"
+str2 = "banana"
+print(str1 == str2) 
+print(str1 < str2)
